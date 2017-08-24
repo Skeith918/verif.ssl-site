fb495b4b8f71aac0da701c8ef45154a4ebc4aea4
Steven <snoussi.steven@gmail.com>
2017-08-24 11:18:01 +0200

    optimize load

--- .getcertinfo.sh
+++ .getcertinfo.sh
@@ -4,20 +4,17 @@ check=$(/usr/bin/ssl-cert-check -b -s $1 -p 443)
 valid=$(echo $check | awk '{print $2}')
 datexpiry=$(echo $check | awk '{print $4,$3,$5}')
 dayleft=$(echo $check | awk '{print $6}')
-
-if grep $1 .certinfo
-then
-  sed -i '/'$1'/d' .certinfo
-  echo $1 $2 $valid $datexpiry $dayleft >> .certinfo
-else
-  echo $1 $2 $valid $datexpiry $dayleft >> .certinfo
-fi
+grade=$(cat .certinfo | grep $1 | awk '{print $8}')
+old_line=$(cat .certinfo | grep $1)
+new_line=$(echo $1 $2 $valid $datexpiry $dayleft $grade)
+sed -i "s/${old_line}/${new_line}/g" .certinfo
 
 while read line; do
   domain=$(echo $line | awk '{print $1}')
+  echo $domain
   if grep $domain domainlist
   then
-    echo 'ok'
+    echo ''
   else
     sed -i '/'$domain'/d' .certinfo
   fi
